be0f77f95d86a8b55e50bb65562d39c4b03dc196
yukov <valeryyukov@gmail.com>
2019-02-11 22:17:40 +0500

    done

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import fetchData from './http';
 
 export default function getLevel(userId) {
   const response = fetchData(`https://server/user/${userId}`);
-
   if (response.status === 'ok') {
     return `Ваш текущий уровень: ${response.level}`;
   }
