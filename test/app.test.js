a235c6bad4b8ef75cfa5517af6580b414d51ddb6
yukov <valeryyukov@gmail.com>
2019-02-12 21:14:42 +0500

    add task 3 test

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ import getLevel from '../src/js/app';
 
 jest.mock('../src/js/http');
 
+
 beforeEach(() => {
   jest.resetAllMocks();
 });
@@ -12,4 +13,16 @@ test('should call getLevel once', () => {
   fetchData.mockReturnValue(JSON.stringify({}));
   getLevel(1);
   expect(fetchData).toBeCalledWith('https://server/user/1');
+});
+
+test('should return level', () => {
+  fetchData.mockReturnValue({ status: 'ok', level: 1 });
+  getLevel(1);
+  expect(getLevel(1)).toBe('Ваш текущий уровень: 1');
+});
+
+test('should return level status error', () => {
+  fetchData.mockReturnValue({ status: 'error', level: 1 });
+  getLevel(1);
+  expect(getLevel(1)).toBe('Информация об уровне временно недоступна');
 });
